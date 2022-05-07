7355d345872734f597c679f87e655218bbd9cc71
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-07 18:39:37 +0100

    rendered list of categories to the page

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tutorial.Data;
+using tutorial.Models;
+
+namespace tutorial.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
