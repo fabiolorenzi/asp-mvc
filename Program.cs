0cd5c06844193b729fed121a13f173ea8f9ccf5d
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-07 17:53:12 +0100

    connected db to program file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using tutorial.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>();
 
 var app = builder.Build();
 
